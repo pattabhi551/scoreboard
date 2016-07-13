cb66b057b88e53bbeea26cbaafc69a3800147d59
Pattabhi Ponugupati <pattabhiponugupati@Pattabhis-MacBook-Pro.local>
2016-07-13 21:47:45 +0200

    working version of paper input

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -70,10 +70,10 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   });
 
   // Scroll page to top and expand header
-  app.scrollPageToTop = function() {
+ /* app.scrollPageToTop = function() {
     app.$.headerPanelMain.scrollToTop(true);
   };
-
+*/
   app.closeDrawer = function() {
     app.$.paperDrawerPanel.closeDrawer();
   };
